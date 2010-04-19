4f410558369112c194735d33e77cc75ba6435eab
SamuraiJack <nickolay8@gmail.com>
2010-04-20 00:28:52 +0400

    - some progress

--- t/010_basics.t.js
+++ t/010_basics.t.js
@@ -11,6 +11,13 @@ StartTest(function(t) {
         
         t.ok(Data.Visitor, "Data.Visitor is here")
         
+        
+        var data1 = {}
+        var data2 = []
+        
+        t.ok(data1 == Data.Visitor.my.visit(data1), "Visiting don't modify the data #1")
+        t.ok(data2 == Data.Visitor.my.visit(data2), "Visiting don't modify the data #2")
+        
         t.endAsync(async0)
     })
 })    
